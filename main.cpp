cb16e57203c8aad286905df64f731748dd056d46
Adrian Dy <ady002@hammer.cs.ucr.edu>
2015-10-25 12:06:50 -0700

    with parse

--- main.cpp
+++ main.cpp
@@ -7,17 +7,43 @@
 #include <sys/wait.h>	//waitpid() and macros
 #include <sys/types.h>
 #include <vector>
+#include <boost/tokenizer.hpp>
+#include <boost/foreach.hpp>
 
 using namespace std;
+using namespace boost;
 
-char** parse(string inputLine);
+vector<string> parse(string inputLine);
 int execute();
 string getInput();
+
+string getInput()
+{
+  string input;
+  getline(cin, input);
+
+  return input;
+}
+
+vector<string> parse(string inputLine)
+{
+  vector<string> vectForTokens;
+  char_separator<char> sep("\t\n");
+  tokenizer<char_separator<char>> tokens(inputLine, sep);
+
+  BOOST_FOREACH(string t, tokens)
+  {
+   vectForTokens.push_back(t);
+   cout<<"inside parse "  << t << endl;  //to check 
+  }
+  
+  return vectForTokens;
+}
 
 void shell()
 {
   string inputLine;
-  char** args;
+  vector<string> args;  //before char** args
   int status;
 
  // do
@@ -28,9 +54,13 @@ void shell()
     else 
       cout << "[" << getlogin() << "@ " << hostName << "]" << "$ ";
    
- //   inputLine = getInput();		//reads user input then saves to inputLine
+    inputLine = getInput();		//reads user input then saves to inputLine
 
- //   args = parse(inputLine);		//parses inputLine for commands to be executed
+    args = parse(inputLine);		//parses inputLine for commands to be executed
+
+    cout << "args "; //to check
+    for(vector<string>::const_iterator it = args.begin(); it != args.end(); it++)
+      cout << *it << " ";	//to check args
 
  //   status = execute(); 		//executes commands and sets status to continue running if 1
 //  }while(status)
