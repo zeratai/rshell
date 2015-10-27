1ebc1a6fc1316bbd7359580889c4ba29eeacbbf8
Adrian Dy <ady002@hammer.cs.ucr.edu>
2015-10-26 18:45:06 -0700

    doesnt work

--- main.cpp
+++ main.cpp
@@ -14,8 +14,83 @@ using namespace std;
 using namespace boost;
 
 vector<string> parse(string inputLine);
-int execute();
+int execute(vector<string> args);
 string getInput();
+
+//written own commands
+int ownCd(vector<string> args);
+int ownExit(vector<string> args);
+
+vector<string> ownCommands = {"cd", "exit"};
+int (*ownCommandFuncs[]) (vector<string>) = {&ownCd, &ownExit};
+
+int ownCd(vector<string> args)
+{
+ if(args.size() < 1)  //checks if only cd is in args as opposed to cd something
+   cout << "need something to cd to" << endl;
+ else
+ { 
+   string forCommand = args[1];
+   char const* command = forCommand.c_str();
+  // char *command = &args[1];	//make args to char** for execvp
+
+   if( chdir(command) != 0 )
+     perror("error in cd"); 
+ } 
+ return 1;
+}
+
+int ownExit(vector<string> args)
+{
+  return 0;
+}
+
+int execute(vector<string> args)
+{
+  if (args.empty() == true)
+    return 1;		//return status of 1
+  else
+  { //check for built in command and execute them
+    vector<string> ownCommands = {"cd", "exit"};
+    for(int i = 0; i < ownCommands.size(); i++)
+    {
+     if( args[0] == ownCommands[i] )
+       return(*ownCommandFuncs[i]) (args); 
+    }
+  }
+  
+  // do execvp
+   pid_t pid;
+   int status;
+    
+   pid = fork();    if (pid < 0)    //error forking
+   if (pid < 0)    //error forking
+   {
+     perror("fork failed");
+     exit(1);	//exit failure
+   }
+   else if (pid == 0)  //child process
+   {
+     char* command[100]; 
+     copy(args.begin(), args.end(), command);
+    // const char* command = forCommand.c_str();
+
+    // char **command = &args[0];	//make args to char** for execvp
+     if(execvp(command[0], command) == -1 )
+       perror("execvp failed");
+
+     exit(1);	//exit failure
+   }
+   else if (pid > 0)  //parent process
+   {
+     do
+     {
+       waitpid(pid, &status, WUNTRACED);
+     }while(!WIFEXITED(status) && !WIFSIGNALED(status));   
+   } 
+
+  return 1;
+}
 
 string getInput()
 {
@@ -46,8 +121,8 @@ void shell()
   vector<string> args;  //before char** args
   int status;
 
- // do
- // {
+    do
+    {
     char hostName[128];			//buffer to hold name from gethostname
     if( (gethostname(hostName, sizeof hostName) == -1) )	//gethostname returns 0=success or -1=error
       perror("gethostname");		//print system error with program name
@@ -62,8 +137,8 @@ void shell()
     for(vector<string>::const_iterator it = args.begin(); it != args.end(); it++)
       cout << *it << " ";	//to check args
 
- //   status = execute(); 		//executes commands and sets status to continue running if 1
-//  }while(status)
+    status = execute(args); 		//executes commands and sets status to continue running if 1
+   }while(status);
 
 }
 
